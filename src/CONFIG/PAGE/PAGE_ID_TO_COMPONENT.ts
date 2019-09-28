b8e4a10d262e9380d23661e41331a78586d9b821
Soulike <316413310@qq.com>
2019-09-28 17:27:56 +0800

    补充 login 和 register 路由配置

--- src/CONFIG/PAGE/PAGE_ID_TO_COMPONENT.ts
+++ src/CONFIG/PAGE/PAGE_ID_TO_COMPONENT.ts
@@ -2,6 +2,8 @@ import {PAGE_ID} from './PAGE_ID';
 
 export const PAGE_ID_TO_COMPONENT = {
     [PAGE_ID.INDEX]: undefined,
+    [PAGE_ID.LOGIN]: undefined,
+    [PAGE_ID.REGISTER]: undefined,
     [PAGE_ID.CREATE_REPOSITORY]: undefined,
     [PAGE_ID.PERSONAL_CENTER]: undefined,
     [PAGE_ID.REPOSITORY]: undefined,
--- src/CONFIG/PAGE/PAGE_ID_TO_ROUTE.ts
+++ src/CONFIG/PAGE/PAGE_ID_TO_ROUTE.ts
@@ -2,6 +2,8 @@ import {PAGE_ID} from './PAGE_ID';
 
 export const PAGE_ID_TO_ROUTE = {
     [PAGE_ID.INDEX]: '/',
+    [PAGE_ID.LOGIN]: '/login',
+    [PAGE_ID.REGISTER]: '/register',
     [PAGE_ID.CREATE_REPOSITORY]: '/createRepository',
     [PAGE_ID.PERSONAL_CENTER]: '/:username',
     [PAGE_ID.REPOSITORY]: '/:username/:repository/:path*',
