ce422ad50a23d743c2736b134ce9b61afb230889
Soulike <316413310@qq.com>
2019-09-29 14:44:23 +0800

    修改仓库列表组件样式
    - RepositoryList 组件添加公私有显示 Tag

--- src/Component/RepositoryList/View.tsx
+++ src/Component/RepositoryList/View.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import Style from './Style.module.scss';
-import {Card, Empty, List} from 'antd';
+import {Card, Empty, List, Tag} from 'antd';
 import {Link} from 'react-router-dom';
 import {PAGE_ID, PAGE_ID_TO_ROUTE} from '../../CONFIG/PAGE';
 import {Repository} from '../../Class';
@@ -20,7 +20,7 @@ function RepositoryList(props: Props)
               locale={{emptyText: <Empty description={'没有仓库'} />}}
               renderItem={repository =>
               {
-                  const {username, name, description} = repository;
+                  const {username, name, description, isPublic} = repository;
                   return (
                       <Link className={Style.repositoryWrapper}
                             target={'__blank'} rel={'noreferrer noopener'}
@@ -29,7 +29,19 @@ function RepositoryList(props: Props)
                                 .replace(':repository', name)
                                 .replace(':path*', '')}>
                           <Card className={Style.repository} hoverable={true}>
-                              <Card.Meta title={`${username} - ${name}`} description={description} />
+                              <Card.Meta title={
+                                  <React.Fragment>
+                                      {
+                                          isPublic ?
+                                              <Tag color={'blue'}>公共</Tag> :
+                                              <Tag color={'gold'}>私人</Tag>
+                                      }
+                                      {username}/{name}
+                                  </React.Fragment>
+                              } description={
+                                  <div>
+                                      {description.length === 0 ? <i>没有描述</i> : description}
+                                  </div>} />
                           </Card>
                       </Link>);
               }} />
