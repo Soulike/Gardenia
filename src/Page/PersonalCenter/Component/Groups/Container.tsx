d4364edf16d1845563d32365e42c582b3c2ad2fb
Soulike <316413310@qq.com>
2019-10-31 15:15:20 +0800

    完成 PersonalCenter 页面组件
        - 完成 PersonalCenter 页面 Groups 组件

--- src/Page/PersonalCenter/Component/Groups/Container.tsx
+++ src/Page/PersonalCenter/Component/Groups/Container.tsx
@@ -1,22 +1,68 @@
 import React, {PureComponent} from 'react';
 import View from './View';
+import {Group} from '../../../../Class';
+import {RouteComponentProps, withRouter} from 'react-router';
+import {Interface as RouterInterface} from '../../../../Router';
+import {Account as AccountApi} from '../../../../Api';
 
-interface IProps
-{
-
-}
+interface IProps extends RouteComponentProps<RouterInterface.IPersonalCenter> {}
 
 interface IState
 {
-
+    groups: Group[],
+    administratingGroups?: Group[],
+    loading: boolean,
 }
 
 class Groups extends PureComponent<IProps, IState>
 {
+    constructor(props: IProps)
+    {
+        super(props);
+        this.state = {
+            groups: [],
+            administratingGroups: [],
+            loading: true,
+        };
+    }
+
+    async componentDidMount()
+    {
+        await Promise.all([
+            this.loadGroups(),
+            this.loadAdministratingGroups(),
+        ]);
+    }
+
+    loadGroups = async () =>
+    {
+        const {match: {params: {username}}} = this.props;
+        this.setState({loading: true});
+        const groups = await AccountApi.getGroups(username);
+        if (groups !== null)
+        {
+            this.setState({groups});
+        }
+        this.setState({loading: false});
+    };
+
+    loadAdministratingGroups = async () =>
+    {
+        const {match: {params: {username}}} = this.props;
+        this.setState({loading: true});
+        const groups = await AccountApi.getAdministratingGroups(username);
+        if (groups !== null)
+        {
+            this.setState({administratingGroups: groups});
+        }
+        this.setState({loading: false});
+    };
+
     render()
     {
-        return (<View />);
+        const {groups, loading, administratingGroups} = this.state;
+        return (<View groups={groups} loading={loading} administratingGroups={administratingGroups} />);
     }
 }
 
-export default Groups;
+export default withRouter(Groups);
--- src/Page/PersonalCenter/Component/Groups/View.tsx
+++ src/Page/PersonalCenter/Component/Groups/View.tsx
@@ -1,11 +1,25 @@
 import React from 'react';
 import Style from './Style.module.scss';
+import {Group} from '../../../../Class';
+import GroupList from '../../../../Component/GroupList';
+import {Alert} from 'antd';
 
-function GroupsView()
+interface IProps
 {
+    groups: Group[],
+    loading: boolean,
+    administratingGroups?: Group[],
+}
+
+function GroupsView(props: IProps)
+{
+    const {groups, loading, administratingGroups} = props;
     return (
         <div className={Style.Groups}>
-            2
+            <div className={Style.tools}>
+                <Alert type={'info'} showIcon={true} message={'可由小组管理员邀请进组'} />
+            </div>
+            <GroupList groups={groups} loading={loading} administratingGroups={administratingGroups} />
         </div>
     );
 }
