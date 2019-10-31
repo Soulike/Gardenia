445f77c38dc284ec1817434adbc22aadb95c3e6c
Soulike <316413310@qq.com>
2019-10-31 12:23:59 +0800

    Router 修改
    - PAGE_ID 修改以防止重复
    - PAGE_ID_TO_COMPONENT 省去定义变量导入的步骤，改为直接动态导入赋值

--- src/Router/CONFIG/PAGE_ID_TO_COMPONENT.ts
+++ src/Router/CONFIG/PAGE_ID_TO_COMPONENT.ts
@@ -1,30 +1,21 @@
 import {PAGE_ID} from './PAGE_ID';
 import React from 'react';
 
-const Login = React.lazy(() => import('../../Page/Login'));
-const Register = React.lazy(() => import('../../Page/Register'));
-const Index = React.lazy(() => import('../../Page/Index'));
-const CreateRepository = React.lazy(() => import('../../Page/CreateRepository'));
-const PersonalCenter = React.lazy(() => import('../../Page/PersonalCenter'));
-
-const Code = React.lazy(() => import('../../Page/Repository/Component/Code'));
-const Options = React.lazy(() => import('../../Page/Repository/Component/Settings/Component/Options'));
-const Groups = React.lazy(() => import('../../Page/Repository/Component/Settings/Component/Groups'));
-
 export const PAGE_ID_TO_COMPONENT = {
-    [PAGE_ID.INDEX]: Index,
-    [PAGE_ID.LOGIN]: Login,
-    [PAGE_ID.REGISTER]: Register,
-    [PAGE_ID.CREATE_REPOSITORY]: CreateRepository,
-    [PAGE_ID.PERSONAL_CENTER]: PersonalCenter,
+    [PAGE_ID.INDEX]: React.lazy(() => import('../../Page/Index')),
+    [PAGE_ID.LOGIN]: React.lazy(() => import('../../Page/Login')),
+    [PAGE_ID.REGISTER]: React.lazy(() => import('../../Page/Register')),
+    [PAGE_ID.CREATE_REPOSITORY]: React.lazy(() => import('../../Page/CreateRepository')),
 
-    [PAGE_ID.REPOSITORY.CODE]: Code,
+    [PAGE_ID.PERSONAL_CENTER]: React.lazy(() => import('../../Page/PersonalCenter')),
+
+    [PAGE_ID.REPOSITORY.CODE]: React.lazy(() => import('../../Page/Repository/Component/Code')),
     [PAGE_ID.REPOSITORY.ISSUES]: undefined,
     [PAGE_ID.REPOSITORY.PULL_REQUESTS]: undefined,
 
-    [PAGE_ID.REPOSITORY.SETTINGS.OPTIONS]: Options,
+    [PAGE_ID.REPOSITORY.SETTINGS.OPTIONS]: React.lazy(() => import('../../Page/Repository/Component/Settings/Component/Options')),
     [PAGE_ID.REPOSITORY.SETTINGS.COLLABORATORS]: undefined,
     [PAGE_ID.REPOSITORY.SETTINGS.BRANCHES]: undefined,
-    [PAGE_ID.REPOSITORY.SETTINGS.GROUPS]: Groups,
+    [PAGE_ID.REPOSITORY.SETTINGS.GROUPS]: React.lazy(() => import('../../Page/Repository/Component/Settings/Component/Groups')),
 
 };
