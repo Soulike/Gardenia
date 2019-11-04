3fa125559c4dccfe9cbc93eae38b186a86d7f392
Soulike <316413310@qq.com>
2019-11-04 18:37:04 +0800

    完成组件
    - 仓库设置页面完成 DeleteRepositoryItem 组件

--- src/Page/Repository/Component/Settings/Component/Options/Component/DangerZoneList/Component/DeleteRepositoryItem/Container.tsx
+++ src/Page/Repository/Component/Settings/Component/Options/Component/DangerZoneList/Component/DeleteRepositoryItem/Container.tsx
@@ -1,26 +1,108 @@
 import React, {PureComponent} from 'react';
 import View from './View';
 import {ButtonProps} from 'antd/lib/button';
-import {Interface as RouterInterface} from '../../../../../../../../../../Router';
+import {Function as RouterFunction, Interface as RouterInterface} from '../../../../../../../../../../Router';
 import {RouteComponentProps, withRouter} from 'react-router-dom';
+import {ModalProps} from 'antd/lib/modal';
+import {InputProps} from 'antd/lib/input';
+import {notification} from 'antd';
+import {Account as AccountClass} from '../../../../../../../../../../Class';
+import {Account as AccountApi, Repository as RepositoryApi} from '../../../../../../../../../../Api';
 
 interface IProps extends RouteComponentProps<RouterInterface.IRepositorySettings> {}
 
 interface IState
 {
-
+    modalRepositoryName: string,
+    modalPassword: string,
+    modalVisible: ModalProps['visible'],
+    modalConfirmLoading: ModalProps['confirmLoading']
 }
 
 class DeleteRepositoryItem extends PureComponent<Readonly<IProps>, IState>
 {
+    constructor(props: Readonly<IProps>)
+    {
+        super(props);
+        this.state = {
+            modalRepositoryName: '',
+            modalPassword: '',
+            modalVisible: false,
+            modalConfirmLoading: false,
+        };
+    }
+
     onButtonClick: ButtonProps['onClick'] = async () =>
     {
+        this.setState({
+            modalRepositoryName: '',
+            modalPassword: '',
+            modalVisible: true,
+        });
+    };
 
+    onModalRepositoryNameInputChange: InputProps['onChange'] = e =>
+    {
+        this.setState({modalRepositoryName: e.target.value});
+    };
+
+    onModalPasswordInputChange: InputProps['onChange'] = e =>
+    {
+        this.setState({modalPassword: e.target.value});
+    };
+
+    onModalOk: ModalProps['onOk'] = async () =>
+    {
+        const {match: {params: {repository, username}}, history} = this.props;
+        const {modalRepositoryName, modalPassword} = this.state;
+        if (repository !== modalRepositoryName)
+        {
+            notification.warn({message: '仓库名不正确'});
+        }
+        else
+        {
+            const hash = AccountClass.calculateHash(username, modalPassword);
+            this.setState({modalConfirmLoading: true});
+            const result = await AccountApi.checkPassword({hash});
+            if (result !== null)
+            {
+                const {isCorrect} = result;
+                if (isCorrect)
+                {
+                    const result = await RepositoryApi.del(repository);
+                    if (result !== null)
+                    {
+                        notification.success({message: '仓库删除成功'});
+                        this.setState({modalVisible: false});
+                        history.replace(RouterFunction.generatePersonalCenterRoute({username}));
+                    }
+                }
+                else
+                {
+                    notification.warn({message: '密码不正确'});
+                }
+            }
+            this.setState({modalConfirmLoading: false});
+        }
+    };
+
+    onModalCancel: ModalProps['onCancel'] = () =>
+    {
+        this.setState({modalVisible: false});
     };
 
     render()
     {
-        return (<View onButtonClick={this.onButtonClick} />);
+        const {modalConfirmLoading, modalVisible, modalPassword, modalRepositoryName} = this.state;
+        return (<View onButtonClick={this.onButtonClick}
+                      modalConfirmLoading={modalConfirmLoading}
+                      modalVisible={modalVisible}
+                      modalPassword={modalPassword}
+                      modalRepositoryName={modalRepositoryName}
+                      onModalOk={this.onModalOk}
+                      onModalCancel={this.onModalCancel}
+                      onModalPasswordInputChange={this.onModalPasswordInputChange}
+                      onModalRepositoryNameInputChange={this.onModalRepositoryNameInputChange} />);
     }
 }
 
--- src/Page/Repository/Component/Settings/Component/Options/Component/DangerZoneList/Component/DeleteRepositoryItem/View.tsx
+++ src/Page/Repository/Component/Settings/Component/Options/Component/DangerZoneList/Component/DeleteRepositoryItem/View.tsx
@@ -1,16 +1,59 @@
 import React from 'react';
 import Item from '../Item';
 import {ButtonProps} from 'antd/lib/button';
+import CriticalOperationConfirmModal from '../../../../../../../../../../Component/CriticalOperationConfirmModal';
+import {InputProps} from 'antd/lib/input';
+import {ModalProps} from 'antd/lib/modal';
 
 interface IProps
 {
-    onButtonClick: ButtonProps['onClick']
+    onButtonClick: ButtonProps['onClick'],
+
+    modalRepositoryName: string,
+    modalPassword: string,
+
+    onModalRepositoryNameInputChange: InputProps['onChange'],
+    onModalPasswordInputChange: InputProps['onChange'],
+
+    modalVisible: ModalProps['visible'],
+    onModalOk: ModalProps['onOk'],
+    onModalCancel: ModalProps['onCancel'],
+    modalConfirmLoading: ModalProps['confirmLoading']
 }
 
 function DeleteRepositoryItemView(props: Readonly<IProps>)
 {
-    const {onButtonClick} = props;
-    return (<Item title={'删除仓库'} description={'删除仓库的操作不可恢复，请慎重。'} onButtonClick={onButtonClick} />);
+    const {
+        onButtonClick,
+        modalRepositoryName,
+        modalPassword,
+        onModalRepositoryNameInputChange,
+        onModalPasswordInputChange,
+        onModalOk,
+        onModalCancel,
+        modalVisible,
+        modalConfirmLoading,
+    } = props;
+    return (
+        <>
+            <Item title={'删除仓库'} description={'删除仓库的操作不可恢复，请慎重。'} onButtonClick={onButtonClick} />
+            <CriticalOperationConfirmModal
+                warning={
+                    <strong>
+                        如果仓库被删除，您将丢失仓库的文件、提交记录等<span style={{color: 'red'}}>所有信息</span>，且删除成功后没有恢复的可能。请在下方输入本仓库的名字和您的密码以确认操作。
+                    </strong>}
+                validateInputPlaceholder={'仓库的名字'}
+                title={'删除仓库确认'}
+                validateText={modalRepositoryName}
+                password={modalPassword}
+                onValidateInputChange={onModalRepositoryNameInputChange}
+                onPasswordInputChange={onModalPasswordInputChange}
+                onOk={onModalOk}
+                onCancel={onModalCancel}
+                visible={modalVisible}
+                confirmLoading={modalConfirmLoading} />
+        </>
+    );
 }
 
 export default React.memo(DeleteRepositoryItemView);
