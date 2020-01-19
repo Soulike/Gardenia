82c11d72d04bb8df67aa235e81c00ea1a965fcb7
Soulike <316413310@qq.com>
2020-01-19 18:55:43 +0800

    完成设置页面头像上传组件

--- src/Page/Setting/Component/Avatar/Container.tsx
+++ src/Page/Setting/Component/Avatar/Container.tsx
@@ -1,21 +1,104 @@
-import React, {PureComponent} from 'react';
+import React, {MouseEventHandler, PureComponent} from 'react';
 import View from './View';
+import {Profile} from '../../../../Api';
+import {InputProps} from 'antd/lib/input';
+import {ButtonProps} from 'antd/lib/button';
+import {notification} from 'antd';
 
-interface IProps
-{
-
-}
+interface IProps {}
 
 interface IState
 {
-
+    avatar: string,
+    loading: boolean,
+    file: File | null
 }
 
 class Avatar extends PureComponent<IProps, IState>
 {
+    fileInputRef = React.createRef<HTMLInputElement>();
+
+    constructor(props: IProps)
+    {
+        super(props);
+        this.state = {
+            avatar: '',
+            loading: false,
+            file: null,
+        };
+    }
+
+    async componentDidMount()
+    {
+        this.setState({loading: true});
+        await this.loadAvatar();
+        this.setState({loading: false});
+    }
+
+    loadAvatar = async () =>
+    {
+        const result = await Profile.get();
+        if (result !== null)
+        {
+            const {avatar} = result;
+            this.setState({avatar});
+        }
+    };
+
+    onAvatarClick: MouseEventHandler<HTMLDivElement> = () =>
+    {
+        const input = this.fileInputRef.current;
+        if (input !== null)
+        {
+            input.click();
+        }
+    };
+
+    onAvatarInputChange: InputProps['onChange'] = e =>
+    {
+        const {files} = e.target;
+        if (files !== null && files.length > 0)
+        {
+            const file = files[0];
+            this.setState({
+                file,
+                avatar: URL.createObjectURL(file),
+            });
+        }
+    };
+
+    onUploadButtonClick: ButtonProps['onClick'] = async () =>
+    {
+        const {file} = this.state;
+        if (file === null)
+        {
+            notification.warn({message: '请先选择头像'});
+        }
+        else
+        {
+            this.setState({loading: true});
+            const result = await Profile.uploadAvatar(file);
+            if (result !== null)
+            {
+                notification.success({message: '上传头像成功'});
+                setTimeout(() =>
+                {
+                    window.location.reload();
+                }, 1000);
+            }
+            this.setState({loading: false});
+        }
+    };
+
     render()
     {
-        return (<View />);
+        const {avatar, loading, file} = this.state;
+        return (<View onAvatarInputChange={this.onAvatarInputChange}
+                      onAvatarClick={this.onAvatarClick}
+                      fileInputRef={this.fileInputRef}
+                      avatar={avatar}
+                      loading={loading} fileChanged={file !== null}
+                      onUploadButtonClick={this.onUploadButtonClick} />);
     }
 }
 
--- src/Page/Setting/Component/Avatar/View.tsx
+++ src/Page/Setting/Component/Avatar/View.tsx
@@ -1,17 +1,53 @@
-import React from 'react';
+import React, {MouseEventHandler} from 'react';
 import Style from './Style.module.scss';
 import SettingsTitle from '../../../../Component/SettingsTitle';
+import DefaultAvatar from '../../../../Component/DefaultAvatar';
+import {Button, Spin, Tooltip} from 'antd';
+import {InputProps} from 'antd/lib/input';
+import {ButtonProps} from 'antd/lib/button';
 
 interface IProps
 {
-
+    avatar: string,
+    loading: boolean,
+    fileChanged: boolean,
+    fileInputRef: React.Ref<HTMLInputElement>,
+    onAvatarClick: MouseEventHandler<HTMLDivElement>,
+    onAvatarInputChange: InputProps['onChange'],
+    onUploadButtonClick: ButtonProps['onClick'],
 }
 
 function Avatar(props: IProps)
 {
+    const {
+        avatar, loading,
+        fileInputRef, onAvatarClick,
+        onAvatarInputChange, fileChanged, onUploadButtonClick,
+    } = props;
     return (
         <div className={Style.Avatar}>
             <SettingsTitle>头像</SettingsTitle>
+            <div className={Style.body}>
+                <Spin spinning={loading}>
+                    <Tooltip title={'点击更换头像'}>
+                        <div className={Style.avatarWrapper} onClick={onAvatarClick}>
+                            {
+                                avatar ? <img src={avatar} alt={'avatar'} className={Style.avatar} /> :
+                                    <DefaultAvatar />
+                            }
+                        </div>
+                    </Tooltip>
+                </Spin>
+                <input type="file" accept={'image/*'} multiple={false}
+                       className={Style.fileInput} ref={fileInputRef}
+                       onChange={onAvatarInputChange} />
+                <div className={Style.buttonWrapper}>
+                    <Button type={'primary'} block={true}
+                            disabled={loading || !fileChanged}
+                            loading={loading}
+                            onClick={onUploadButtonClick}>上传</Button>
+                </div>
+            </div>
         </div>
     );
 }
