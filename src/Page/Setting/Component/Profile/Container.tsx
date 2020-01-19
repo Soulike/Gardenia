006f101d460a78e71243aa2efe5ae332550cf920
Soulike <316413310@qq.com>
2020-01-19 16:36:22 +0800

    完成页面
    - 完成设置中个人资料设置页面业务逻辑

--- src/Page/Setting/Component/Profile/Container.tsx
+++ src/Page/Setting/Component/Profile/Container.tsx
@@ -1,21 +1,92 @@
 import React, {PureComponent} from 'react';
 import View from './View';
+import {InputProps} from 'antd/lib/input';
+import {ButtonProps} from 'antd/lib/button';
+import {Profile as ProfileApi} from '../../../../Api';
+import {notification} from 'antd';
 
-interface IProps
-{
-
-}
+interface IProps {}
 
 interface IState
 {
-
+    nickname: string,
+    email: string,
+    loading: boolean,
 }
 
 class Profile extends PureComponent<IProps, IState>
 {
+    constructor(props: IProps)
+    {
+        super(props);
+        this.state = {
+            nickname: '',
+            email: '',
+            loading: false,
+        };
+    }
+
+    async componentDidMount()
+    {
+        await this.loadProfile();
+    }
+
+    loadProfile = async () =>
+    {
+        this.setState({loading: true});
+        const result = await ProfileApi.get();
+        if (result !== null)
+        {
+            const {nickname, email} = result;
+            this.setState({nickname, email, loading: false});
+        }
+    };
+
+    onNicknameInputChange: InputProps['onChange'] = e =>
+    {
+        this.setState({nickname: e.target.value});
+    };
+
+    onEmailInputChange: InputProps['onChange'] = e =>
+    {
+        this.setState({email: e.target.value});
+    };
+
+    onNicknameSubmit: ButtonProps['onClick'] = async () =>
+    {
+        const {nickname} = this.state;
+        this.setState({loading: true});
+        const result = await ProfileApi.set({nickname});
+        if (result !== null)
+        {
+            notification.success({message: '昵称修改成功'});
+        }
+        this.setState({loading: false});
+    };
+
+    onEmailSubmit: ButtonProps['onClick'] = async () =>
+    {
+        const {email} = this.state;
+        this.setState({loading: true});
+        const result = await ProfileApi.set({email});
+        if (result !== null)
+        {
+            notification.success({message: '邮箱修改成功'});
+        }
+        this.setState({loading: false});
+    };
+
     render()
     {
-        return (<View />);
+        const {nickname, email, loading} = this.state;
+        return (
+            <View nickname={nickname}
+                  email={email}
+                  loading={loading}
+                  onEmailInputChange={this.onEmailInputChange}
+                  onEmailSubmit={this.onEmailSubmit}
+                  onNicknameInputChange={this.onNicknameInputChange}
+                  onNicknameSubmit={this.onNicknameSubmit} />);
     }
 }
 
--- src/Page/Setting/Component/Profile/View.tsx
+++ src/Page/Setting/Component/Profile/View.tsx
@@ -3,23 +3,43 @@ import Style from './Style.module.scss';
 import SettingsTitle from '../../../../Component/SettingsTitle';
 import SettingLabel from '../../../../Component/SettingLabel';
 import {Button, Input} from 'antd';
+import {InputProps} from 'antd/lib/input';
+import {ButtonProps} from 'antd/lib/button';
 
 interface IProps
 {
+    nickname: string,
+    email: string,
+    onNicknameInputChange: InputProps['onChange'],
+    onEmailInputChange: InputProps['onChange'],
+    onNicknameSubmit: ButtonProps['onClick'],
+    onEmailSubmit: ButtonProps['onClick'],
 
+    loading: boolean,
 }
 
 function Profile(props: IProps)
 {
+    const {
+        nickname, email,
+        onEmailInputChange, onNicknameInputChange,
+        onEmailSubmit, onNicknameSubmit,
+        loading,
+    } = props;
     return (
         <div className={Style.Profile}>
             <SettingsTitle>个人资料</SettingsTitle>
             <div className={Style.item}>
                 <SettingLabel>昵称</SettingLabel>
                 <div className={Style.form}>
-                    <div className={Style.inputWrapper}><Input /></div>
+                    <div className={Style.inputWrapper}>
+                        <Input onChange={onNicknameInputChange} value={nickname} disabled={loading} />
+                    </div>
                     <div className={Style.saveButtonWrapper}>
-                        <Button type={'primary'}>保存</Button>
+                        <Button type={'primary'}
+                                onClick={onNicknameSubmit}
+                                loading={loading}
+                                disabled={loading}>保存</Button>
                     </div>
                 </div>
             </div>
@@ -27,10 +47,13 @@ function Profile(props: IProps)
                 <SettingLabel>邮箱</SettingLabel>
                 <div className={`${Style.form} ${Style.emailForm}`}>
                     <div className={`${Style.inputWrapper} ${Style.emailInputWrapper}`}>
-                        <Input type={'email'} />
+                        <Input type={'email'} onChange={onEmailInputChange} value={email} disabled={loading} />
                     </div>
                     <div className={Style.saveButtonWrapper}>
-                        <Button type={'primary'}>保存</Button>
+                        <Button type={'primary'}
+                                onClick={onEmailSubmit}
+                                loading={loading}
+                                disabled={loading}>保存</Button>
                     </div>
                 </div>
             </div>
