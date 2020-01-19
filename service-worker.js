bb6cad2f27d6da3f92d3a3508d92efa48e7a3dd7
Soulike <316413310@qq.com>
2020-01-19 18:56:56 +0800

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.36acc794c6ab6f149c2909053d2b4df4.js"
+  "/precache-manifest.c687b7c48df0847408452c6c1bf00235.js"
 );
 
 self.addEventListener('message', (event) => {
