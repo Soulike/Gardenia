c1da7ec8025026db19e6389ba869c8c51b632211
Soulike <316413310@qq.com>
2020-04-02 14:58:44 +0800

    PR 添加评论功能

--- src/Page/Repository/Page/PullRequest/Component/Comments/View.tsx
+++ src/Page/Repository/Page/PullRequest/Component/Comments/View.tsx
@@ -6,10 +6,11 @@ import {Alert, Spin} from 'antd';
 import {PULL_REQUEST_STATUS} from '../../../../../../CONSTANT';
 import OperationButton from './Component/OperationButton';
 import {LoadingOutlined} from '@ant-design/icons';
+import CommentPoster from './Component/CommentPoster';
 
 interface IProps
 {
-    pullRequest: Pick<PullRequest, 'sourceRepositoryUsername' | 'creationTime' | 'modificationTime' | 'content' | 'status'>;
+    pullRequest: Pick<PullRequest, 'id' | 'sourceRepositoryUsername' | 'creationTime' | 'modificationTime' | 'content' | 'status'>;
     pullRequestComments: PullRequestComment[];
     loading: boolean;
     isMergeable: boolean;
@@ -19,44 +20,53 @@ function Comments(props: IProps)
 {
     const {
         pullRequest: {sourceRepositoryUsername, modificationTime, content, creationTime, status},
+        pullRequest,
         pullRequestComments, loading, isMergeable,
     } = props;
     return (
         <div className={Style.Comments}>
             <Spin spinning={loading}>
-                <div className={Style.commentWrapper}>
-                    <Comment topicCreatorUsername={sourceRepositoryUsername}
-                             username={sourceRepositoryUsername}
-                             content={content}
-                             creationTime={creationTime}
-                             modificationTime={modificationTime} />
-                </div>
                 {
-                    pullRequestComments.map(({id, username, content, creationTime, modificationTime}) => (
-                        <div className={Style.commentWrapper} key={id}>
-                            <Comment topicCreatorUsername={sourceRepositoryUsername}
-                                     username={username}
-                                     content={content}
-                                     creationTime={creationTime}
-                                     modificationTime={modificationTime} />
+                    status === PULL_REQUEST_STATUS.OPEN ? (
+                        <div className={Style.isMergeableWrapper}>
+                            {
+                                loading ? <Alert type={'warning'}
+                                                 icon={<LoadingOutlined />}
+                                                 showIcon={true}
+                                                 message={'正在检查是否可以自动合并'} /> :
+                                    isMergeable
+                                        ? <Alert type={'success'} showIcon={true} message={'没有冲突，可以自动合并'} />
+                                        : <Alert type={'error'} showIcon={true} message={'存在冲突，不能自动合并'} />
+                            }
                         </div>
-                    ))
+                    ) : null
                 }
+                <div className={Style.commentsWrapper}>
+                    <div className={Style.commentWrapper}>
+                        <Comment topicCreatorUsername={sourceRepositoryUsername}
+                                 username={sourceRepositoryUsername}
+                                 content={content}
+                                 creationTime={creationTime}
+                                 modificationTime={modificationTime} />
+                    </div>
+                    {
+                        pullRequestComments.map(({id, username, content, creationTime, modificationTime}) => (
+                            <div className={Style.commentWrapper} key={id}>
+                                <Comment topicCreatorUsername={sourceRepositoryUsername}
+                                         username={username}
+                                         content={content}
+                                         creationTime={creationTime}
+                                         modificationTime={modificationTime} />
+                            </div>
+                        ))
+                    }
+                </div>
             </Spin>
             {
-                status === PULL_REQUEST_STATUS.OPEN ? (
-                    <div className={Style.isMergeableWrapper}>
-                        {
-                            loading ? <Alert type={'warning'}
-                                             icon={<LoadingOutlined />}
-                                             showIcon={true}
-                                             message={'正在检查是否可以自动合并'} /> :
-                                isMergeable
-                                    ? <Alert type={'success'} showIcon={true} message={'没有冲突，可以自动合并'} />
-                                    : <Alert type={'error'} showIcon={true} message={'存在冲突，不能自动合并'} />
-                        }
-                    </div>
-                ) : null
+                status === PULL_REQUEST_STATUS.OPEN ?
+                    (<div className={Style.commentPosterWrapper}>
+                        <CommentPoster pullRequest={pullRequest} loading={loading} />
+                    </div>) : null
             }
             <div className={Style.operationButtonWrapper}>
                 <OperationButton isMergeable={isMergeable} />
