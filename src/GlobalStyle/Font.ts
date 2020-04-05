837f37d01c2f98c4ea1ec0d9286d43f72f186859
Soulike <316413310@qq.com>
2020-04-05 16:13:32 +0800

    修复字体未打包的 bug

---
+++ src/GlobalStyle/Font.ts
@@ -0,0 +1,13 @@
+import '../Static/Font/SourceCodePro/SourceCodePro-ExtraLight.otf.woff2';
+import '../Static/Font/SourceCodePro/SourceCodePro-ExtraLightIt.otf.woff2';
+import '../Static/Font/SourceCodePro/SourceCodePro-Light.otf.woff2';
+import '../Static/Font/SourceCodePro/SourceCodePro-LightIt.otf.woff2';
+import '../Static/Font/SourceCodePro/SourceCodePro-Regular.otf.woff2';
+import '../Static/Font/SourceCodePro/SourceCodePro-Medium.otf.woff2';
+import '../Static/Font/SourceCodePro/SourceCodePro-MediumIt.otf.woff2';
+import '../Static/Font/SourceCodePro/SourceCodePro-Semibold.otf.woff2';
+import '../Static/Font/SourceCodePro/SourceCodePro-SemiboldIt.otf.woff2';
+import '../Static/Font/SourceCodePro/SourceCodePro-Bold.otf.woff2';
+import '../Static/Font/SourceCodePro/SourceCodePro-BoldIt.otf.woff2';
+import '../Static/Font/SourceCodePro/SourceCodePro-Black.otf.woff2';
+import '../Static/Font/SourceCodePro/SourceCodePro-BlackIt.otf.woff2';
--- src/GlobalStyle/index.ts
+++ src/GlobalStyle/index.ts
@@ -1,1 +1,2 @@
 import './Global.scss';
+import './Font';
