d79c1a299c55115f68c1b33a5161906059129e6e
Soulike <316413310@qq.com>
2020-05-12 17:38:45 +0800

    修复 bug
    - 设定退出登录后页面跳转到首页

--- src/Component/Root/Component/LoggedInMenu/Container.tsx
+++ src/Component/Root/Component/LoggedInMenu/Container.tsx
@@ -1,4 +1,3 @@
-import {AnyAction} from 'redux';
 import React, {PureComponent} from 'react';
 import {PopconfirmProps} from 'antd/lib/popconfirm';
 import {Account} from '../../../../Api/Account';
@@ -6,26 +5,31 @@ import {notification} from 'antd';
 import View from './View';
 import {setLoggedOutAction} from '../../Action/Action';
 import {connect} from 'react-redux';
+import {RouteComponentProps, withRouter} from 'react-router-dom';
+import {CONFIG} from '../../../../Router';
+
+const {PAGE_ID, PAGE_ID_TO_ROUTE} = CONFIG;
 
 interface IState {}
 
-interface IProps
+interface IProps extends RouteComponentProps
 {
     username: string,
     avatar: string,
-    setLoggedOut: () => AnyAction;
+    setLoggedOut: typeof setLoggedOutAction;
 }
 
 class LoggedInMenu extends PureComponent<IProps, IState>
 {
     onLogoutClick: PopconfirmProps['onConfirm'] = async () =>
     {
-        const {setLoggedOut} = this.props;
+        const {setLoggedOut, history} = this.props;
         const result = await Account.logout();
         if (result !== null)
         {
             setLoggedOut();
             notification.success({message: '退出登录成功'});
+            history.replace(PAGE_ID_TO_ROUTE[PAGE_ID.INDEX]);
         }
     };
 
@@ -45,4 +49,4 @@ const mapDispatchToProps = {
     setLoggedOut: setLoggedOutAction,
 };
 
-export default connect(null, mapDispatchToProps)(LoggedInMenu);
+export default connect(null, mapDispatchToProps)(withRouter(LoggedInMenu));
