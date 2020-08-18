cc9ceb01f378d689bff22248a05124b2c55286f8
Soulike <316413310@qq.com>
2020-08-18 17:38:02 +0800

    Root 登录后菜单添加消息中心徽标，修复 Api 层设计错误，删除了多余组件

--- src/Component/Root/Component/LoggedInMenu/Component/MenuDivider/View.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import Style from './Style.module.scss';
-
-function MenuDivider()
-{
-    return (<div className={Style.MenuDivider} />);
-}
-
-export default React.memo(MenuDivider);
--- src/Component/Root/Component/LoggedInMenu/Container.tsx
+++ src/Component/Root/Component/LoggedInMenu/Container.tsx
@@ -1,16 +1,20 @@
 import React, {PureComponent} from 'react';
 import {PopconfirmProps} from 'antd/lib/popconfirm';
-import {Account} from '../../../../Api/Account';
+import {Account, Notification as NotificationApi} from '../../../../Api';
 import {notification} from 'antd';
 import View from './View';
 import {setLoggedOutAction} from '../../Action/Action';
 import {connect} from 'react-redux';
 import {RouteComponentProps, withRouter} from 'react-router-dom';
 import {CONFIG} from '../../../../Router';
+import {promisify} from 'util';
 
 const {PAGE_ID, PAGE_ID_TO_ROUTE} = CONFIG;
 
-interface IState {}
+interface IState
+{
+    unconfirmedNotificationCount: number,
+}
 
 interface IProps extends RouteComponentProps
 {
@@ -21,6 +25,31 @@ interface IProps extends RouteComponentProps
 
 class LoggedInMenu extends PureComponent<IProps, IState>
 {
+    private setStatePromise = promisify(this.setState);
+
+    constructor(props: IProps)
+    {
+        super(props);
+        this.state = {
+            unconfirmedNotificationCount: 0,
+        };
+    }
+
+    async componentDidMount()
+    {
+        await this.loadNotificationCount();
+    }
+
+    loadNotificationCount = async () =>
+    {
+        const countWrapper = await NotificationApi.getCount({confirmed: false});
+        if (countWrapper !== null)
+        {
+            const {count} = countWrapper;
+            await this.setStatePromise({unconfirmedNotificationCount: count});
+        }
+    };
+
     onLogoutClick: PopconfirmProps['onConfirm'] = async () =>
     {
         const {setLoggedOut, history} = this.props;
@@ -36,10 +65,12 @@ class LoggedInMenu extends PureComponent<IProps, IState>
     render()
     {
         const {username, avatar} = this.props;
+        const {unconfirmedNotificationCount} = this.state;
         return (
             <View
                 username={username}
                 avatar={avatar}
+                unconfirmedNotificationCount={unconfirmedNotificationCount}
                 onLogoutClick={this.onLogoutClick} />
         );
     }
