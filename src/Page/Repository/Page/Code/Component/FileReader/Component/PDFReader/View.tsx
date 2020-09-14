f8566375724d17c27734bd0d9caf1d51e0e1b975
Soulike <316413310@qq.com>
2020-09-14 21:44:06 +0800

    完善 PDFReader
    - 修复了 PDFReader 下方无用的空白

--- src/Page/Repository/Page/Code/Component/FileReader/Component/PDFReader/View.tsx
+++ src/Page/Repository/Page/Code/Component/FileReader/Component/PDFReader/View.tsx
@@ -25,7 +25,7 @@ function PDFReader(props: IProps)
                 loading={<Spin spinning={true} />}
                 error={<Alert type={'error'} message={<div><ExclamationCircleOutlined /> PDF 文件加载失败</div>} />}
                 onLoadSuccess={onPDFLoadSuccess}>
-                <Page pageNumber={pageNumber} />
+                <Page pageNumber={pageNumber} renderAnnotationLayer={false} />
             </Document>
             <div className={Style.control}>
                 <Pagination pageSize={1}
