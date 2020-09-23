e1fd6b5750fb611f2d2da3d31b538c07a3e42520
Soulike <316413310@qq.com>
2020-09-23 18:31:48 +0800

    修复 react-pdf 显示错误

--- src/Page/Repository/Page/Code/Component/FileReader/Component/PDFReader/View.tsx
+++ src/Page/Repository/Page/Code/Component/FileReader/Component/PDFReader/View.tsx
@@ -2,9 +2,8 @@ import React from 'react';
 import {Document, Page} from '../../../../../../../../Component/react-pdf';
 import {Props as DocumentProps} from 'react-pdf/dist/Document';
 import Style from './Style.module.scss';
-import {Alert, Pagination, Spin} from 'antd';
+import {Pagination, Spin} from 'antd';
 import {PaginationProps} from 'antd/lib/pagination';
-import {ExclamationCircleOutlined} from '@ant-design/icons';
 
 interface IProps
 {
@@ -23,7 +22,7 @@ function PDFReader(props: IProps)
             <Document
                 file={pdfURL}
                 loading={<Spin spinning={true} />}
-                error={<Alert type={'error'} message={<div><ExclamationCircleOutlined /> PDF 文件加载失败</div>} />}
+                error={<Spin spinning={true} />}
                 onLoadSuccess={onPDFLoadSuccess}>
                 <Page pageNumber={pageNumber} />
             </Document>
