59504a41971db9343f391b7768b5952b2adfa47e
Soulike <316413310@qq.com>
2020-09-27 18:48:11 +0800

    使用 hooks 重构 Repository 页面

--- src/Page/Repository/Container.tsx
+++ src/Page/Repository/Container.tsx
@@ -1,249 +1,216 @@
-import React, {PureComponent, ReactNode} from 'react';
+import React, {ReactNode, useEffect, useLayoutEffect, useState} from 'react';
 import View from './View';
-import {RouteComponentProps} from 'react-router-dom';
+import {useHistory, useParams, useRouteMatch} from 'react-router-dom';
 import {Profile, Repository as RepositoryClass} from '../../Class';
 import {Issue as IssueApi, Profile as ProfileApi, PullRequest as PullRequestApi, RepositoryInfo} from '../../Api';
 import {Function as RouterFunction, Interface as RouterInterface} from '../../Router';
-import {TabsProps} from 'antd/lib/tabs';
 import TAB_KEY from './TAB_KEY';
-import {connect} from 'react-redux';
+import {useSelector} from 'react-redux';
 import {IRootState, IState as StoreState} from '../../Store';
 import * as CONFIG from '../../CONFIG';
 import {PAGE_ID, PAGE_ID_TO_ROUTE} from '../../CONFIG';
 import {ISSUE_STATUS, PULL_REQUEST_STATUS} from '../../CONSTANT';
+import {TabsProps} from 'antd/lib/tabs';
 
-interface IProps extends RouteComponentProps<RouterInterface.IRepositoryCode
-    & RouterInterface.IRepositoryIssues
-    & RouterInterface.IRepositoryPullRequests
-    & RouterInterface.IRepositorySettings>
+interface IProps
 {
-    isLoggedIn: IRootState['isLoggedIn'],
     children: ReactNode
 }
 
-interface IState
+function Repository(props: IProps)
 {
-    repository: RepositoryClass,
-    loading: boolean,
-    tabActiveKey: TAB_KEY,
-    visitorProfile: Profile | null,
-    forkFrom: Readonly<Pick<RepositoryClass, 'username' | 'name'>> | null;
-    openPullRequestAmount: number;
-    openIssueAmount: number;
-}
+    const [repository, setRepository] = useState(new RepositoryClass('', '', '', true));
+    const [loading, setLoading] = useState(false);
+    const [tabActiveKey, setTabActiveKey] = useState(TAB_KEY.CODE);
+    const [visitorProfile, setVisitorProfile] = useState<Profile | null>(null);
+    const [forkFrom, setForkFrom] = useState<Readonly<Pick<RepositoryClass, 'username' | 'name'>> | null>(null);
+    const [openPullRequestAmount, setOpenPullRequestAmount] = useState(0);
+    const [openIssueAmount, setOpenIssueAmount] = useState(0);
 
-class Repository extends PureComponent<Readonly<IProps>, IState>
-{
-    constructor(props: Readonly<IProps>)
+    const history = useHistory();
+    const {objectType, commitHash, username, repositoryName} = useParams<RouterInterface.IRepositoryCode
+        & RouterInterface.IRepositoryIssues
+        & RouterInterface.IRepositoryPullRequests
+        & RouterInterface.IRepositorySettings>();
+    const {path} = useRouteMatch<RouterInterface.IRepositoryCode
+        & RouterInterface.IRepositoryIssues
+        & RouterInterface.IRepositoryPullRequests
+        & RouterInterface.IRepositorySettings>();
+    const {isLoggedIn} = useSelector<StoreState, IRootState>(({Root}) => Root);
+
+    // 检查 URL 合法性
+    useEffect(() =>
     {
-        super(props);
-        this.checkURLParameter();
-        this.state = {
-            repository: new RepositoryClass('', '', '', true),
-            loading: true,
-            tabActiveKey: TAB_KEY.CODE,
-            visitorProfile: null,
-            forkFrom: null,
-            openPullRequestAmount: 0,
-            openIssueAmount: 0,
+        const checkURLParameter = () =>
+        {
+            // 只要 objectType 存在，commitHash 一定存在。如果不满足则跳转到 404
+            if (objectType !== undefined && commitHash === undefined)
+            {
+                history.replace(PAGE_ID_TO_ROUTE[PAGE_ID.NOT_FOUND]);
+            }
         };
-    }
 
+        checkURLParameter();
+    }, [objectType, commitHash, history]);
 
-    async componentDidMount()
+    // 设置页面标题
+    useEffect(() =>
     {
-        const {isLoggedIn} = this.props;
-        this.setTitle();
-        this.setTabActiveKey();
-        this.setState({loading: true});
-        await Promise.all([
-            this.loadRepository(),
-            this.loadForkFrom(),
-            this.loadOpenPullRequestAmount(),
-            this.loadOpenIssueAmount(),
-        ]);
-        if (isLoggedIn)
-        {
-            await this.loadVisitorProfile();
-        }
-        this.setState({loading: false});
-    }
+        document.title = `${username}/${repositoryName} - ${CONFIG.NAME}`;
+    }, [username, repositoryName]);
 
-    async componentDidUpdate(prevProps: Readonly<IProps>, prevState: Readonly<IState>, snapshot?: any)
+    // 设置当前的 TAB
+    useLayoutEffect(() =>
     {
-        const {
-            location: {pathname},
-            isLoggedIn, match:
-                {params: {repositoryName, username}},
-        } = this.props;
-        const {
-            location: {pathname: prevPathName},
-            isLoggedIn: preIsLoggedIn,
-            match: {params: {repositoryName: prevRepository, username: prevUsername}},
-        } = prevProps;
-        if (repositoryName !== prevRepository || username !== prevUsername)
-        {
-            await this.componentDidMount();
-        }
-        if (pathname !== prevPathName)
-        {
-            this.setTabActiveKey();
-        }
-        if (isLoggedIn && !preIsLoggedIn)
-        {
-            await this.loadVisitorProfile();
-        }
-    }
-
-    checkURLParameter = () =>
-    {
-        // 只要 objectType 存在，commitHash 一定存在。如果不满足则跳转到 404
-        const {
-            history, match: {
-                params: {
-                    objectType, commitHash,
-                },
-            },
-        } = this.props;
-        if (objectType !== undefined && commitHash === undefined)
-        {
-            history.replace(PAGE_ID_TO_ROUTE[PAGE_ID.NOT_FOUND]);
-        }
-    };
-
-    setTitle = () =>
-    {
-        const {match: {params: {username, repositoryName: name}}} = this.props;
-        document.title = `${username}/${name} - ${CONFIG.NAME}`;
-    };
-
-    loadOpenIssueAmount = async () =>
-    {
-        const {match: {params: {username, repositoryName: name}}} = this.props;
-        const amountWrapper = await IssueApi.getAmountByRepository(
-            {username, name}, ISSUE_STATUS.OPEN,
-        );
-        if (amountWrapper !== null)
-        {
-            const {amount} = amountWrapper;
-            this.setState({openIssueAmount: amount});
-        }
-    };
-
-    loadOpenPullRequestAmount = async () =>
-    {
-        const {match: {params: {username, repositoryName: name}}} = this.props;
-        const amountWrapper = await PullRequestApi.getPullRequestAmount({username, name}, PULL_REQUEST_STATUS.OPEN);
-        if (amountWrapper !== null)
-        {
-            const {amount} = amountWrapper;
-            this.setState({openPullRequestAmount: amount});
-        }
-    };
-
-    loadRepository = async () =>
-    {
-        const {match: {params: {username, repositoryName}}, history} = this.props;
-        const repository = await RepositoryInfo.repository({username}, {name: repositoryName});
-        // 设置仓库基本信息
-        if (repository !== null)
-        {
-            this.setState({repository});
-        }
-        else
-        {
-            return history.replace(PAGE_ID_TO_ROUTE[PAGE_ID.NOT_FOUND]);
-        }
-    };
-
-    loadForkFrom = async () =>
-    {
-        const {match: {params: {username, repositoryName}}} = this.props;
-        const result = await RepositoryInfo.forkFrom({username, name: repositoryName});
-        if (result !== null)
-        {
-            const {repository} = result;
-            this.setState({forkFrom: repository});
-        }
-    };
-
-    loadVisitorProfile = async () =>
-    {
-        const visitorProfile = await ProfileApi.get();
-        if (visitorProfile !== null)
-        {
-            this.setState({
-                visitorProfile,
-            });
-        }
-    };
-
-    setTabActiveKey = () =>
-    {
-        const {match: {path}} = this.props;
         const {REPOSITORY: {REPOSITORY, CODE, ISSUES, ISSUE, CREATE_ISSUE, PULL_REQUESTS, PULL_REQUEST, SETTINGS, CONFLICT}} = PAGE_ID;
         switch (path)
         {
             case PAGE_ID_TO_ROUTE[CODE]:
             case PAGE_ID_TO_ROUTE[REPOSITORY]:
             {
-                this.setState({
-                    tabActiveKey: TAB_KEY.CODE,
-                });
+                setTabActiveKey(TAB_KEY.CODE);
                 break;
             }
             case PAGE_ID_TO_ROUTE[CREATE_ISSUE]:
             case PAGE_ID_TO_ROUTE[ISSUE]:
             case PAGE_ID_TO_ROUTE[ISSUES]:
             {
-                this.setState({
-                    tabActiveKey: TAB_KEY.ISSUES,
-                });
+                setTabActiveKey(TAB_KEY.ISSUES);
                 break;
             }
             case PAGE_ID_TO_ROUTE[PULL_REQUEST]:
             case PAGE_ID_TO_ROUTE[PULL_REQUESTS]:
             case PAGE_ID_TO_ROUTE[CONFLICT]:
             {
-                this.setState({
-                    tabActiveKey: TAB_KEY.PULL_REQUESTS,
-                });
+                setTabActiveKey(TAB_KEY.PULL_REQUESTS);
                 break;
             }
             case PAGE_ID_TO_ROUTE[SETTINGS.SETTINGS]:
             {
-                this.setState({
-                    tabActiveKey: TAB_KEY.SETTINGS,
-                });
+                setTabActiveKey(TAB_KEY.SETTINGS);
                 break;
             }
         }
-    };
+    }, [path]);
 
-    onTabChange: TabsProps['onChange'] = activeKey =>
+    // 加载仓库基本信息
+    useEffect(() =>
     {
-        const {history} = this.props;
-        const {repository: {username, name}} = this.state;
+        const loadRepository = async () =>
+        {
+            const repository = await RepositoryInfo.repository({username}, {name: repositoryName});
+            // 设置仓库基本信息
+            if (repository !== null)
+            {
+                setRepository(repository);
+            }
+            else
+            {
+                return history.replace(PAGE_ID_TO_ROUTE[PAGE_ID.NOT_FOUND]);
+            }
+        };
+
+        setLoading(true);
+        loadRepository().finally(() => setLoading(false));
+    }, [username, repositoryName, history]);
+
+    // 加载 fork 信息
+    useEffect(() =>
+    {
+        const loadForkFrom = async () =>
+        {
+            const result = await RepositoryInfo.forkFrom({username, name: repositoryName});
+            if (result !== null)
+            {
+                const {repository} = result;
+                setForkFrom(repository);
+            }
+        };
+
+        setLoading(true);
+        loadForkFrom().finally(() => setLoading(false));
+    }, [username, repositoryName]);
+
+    // 加载 Open PR 的数量
+    useEffect(() =>
+    {
+        const loadOpenPullRequestAmount = async () =>
+        {
+            const amountWrapper = await PullRequestApi.getPullRequestAmount({
+                username,
+                name: repositoryName,
+            }, PULL_REQUEST_STATUS.OPEN);
+            if (amountWrapper !== null)
+            {
+                const {amount} = amountWrapper;
+                setOpenPullRequestAmount(amount);
+            }
+        };
+
+        setLoading(true);
+        loadOpenPullRequestAmount().finally(() => setLoading(false));
+    }, [username, repositoryName]);
+
+    // 加载 Open Issue 的数量
+    useEffect(() =>
+    {
+        const loadOpenIssueAmount = async () =>
+        {
+            const amountWrapper = await IssueApi.getAmountByRepository(
+                {username, name: repositoryName}, ISSUE_STATUS.OPEN,
+            );
+            if (amountWrapper !== null)
+            {
+                const {amount} = amountWrapper;
+                setOpenIssueAmount(amount);
+            }
+        };
+
+        setLoading(true);
+        loadOpenIssueAmount().finally(() => setLoading(false));
+    }, [username, repositoryName]);
+
+    // 加载 visitorProfile
+    useEffect(() =>
+    {
+        const loadVisitorProfile = async () =>
+        {
+            const visitorProfile = await ProfileApi.get();
+            if (visitorProfile !== null)
+            {
+                setVisitorProfile(visitorProfile);
+            }
+        };
+
+        if (isLoggedIn)
+        {
+            setLoading(true);
+            loadVisitorProfile().finally(() => setLoading(false));
+        }
+    }, [isLoggedIn]);
+
+    const onTabChange: TabsProps['onChange'] = activeKey =>
+    {
         switch (activeKey)
         {
             case TAB_KEY.CODE:
             {
-                return history.push(RouterFunction.generateRepositoryCodeRoute({username, repositoryName: name}));
+                return history.push(RouterFunction.generateRepositoryCodeRoute({username, repositoryName}));
             }
             case TAB_KEY.ISSUES:
             {
-                return history.push(RouterFunction.generateRepositoryIssuesRoute({username, repositoryName: name}));
+                return history.push(RouterFunction.generateRepositoryIssuesRoute({username, repositoryName}));
             }
             case TAB_KEY.PULL_REQUESTS:
             {
                 return history.push(RouterFunction.generateRepositoryPullRequestsRoute({
                     username,
-                    repositoryName: name,
+                    repositoryName,
                 }));
             }
             case TAB_KEY.SETTINGS:
             {
-                return history.push(RouterFunction.generateRepositorySettingsRoute({username, repositoryName: name}));
+                return history.push(RouterFunction.generateRepositorySettingsRoute({username, repositoryName}));
             }
             default:
             {
@@ -252,32 +219,18 @@ class Repository extends PureComponent<Readonly<IProps>, IState>
         }
     };
 
-    render()
-    {
-        const {
-            repository, loading, tabActiveKey, visitorProfile, forkFrom,
-            openPullRequestAmount, openIssueAmount,
-        } = this.state;
-        const {children, isLoggedIn} = this.props;
-        return (
-            <View showSettings={isLoggedIn && visitorProfile !== null && repository.username === visitorProfile.username}
-                  repository={repository}
-                  loading={loading}
-                  onTabChange={this.onTabChange}
-                  tabActiveKey={tabActiveKey}
-                  forkFrom={forkFrom}
-                  openPullRequestAmount={openPullRequestAmount}
-                  openIssueAmount={openIssueAmount}>
-                {children}
-            </View>
-        );
-    }
+    const {children} = props;
+    return (
+        <View showSettings={isLoggedIn && visitorProfile !== null && repository.username === visitorProfile.username}
+              repository={repository}
+              loading={loading}
+              onTabChange={onTabChange}
+              tabActiveKey={tabActiveKey}
+              forkFrom={forkFrom}
+              openPullRequestAmount={openPullRequestAmount}
+              openIssueAmount={openIssueAmount}>
+            {children}
+        </View>);
 }
 
-const mapStateToProps = (state: StoreState) =>
-{
-    const {Root: {isLoggedIn}} = state;
-    return {isLoggedIn};
-};
-
-export default connect(mapStateToProps)(Repository);
+export default React.memo(Repository);
